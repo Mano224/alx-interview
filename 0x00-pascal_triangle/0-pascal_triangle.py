9cb9614302788c7739b47081e2289329706c1bbb
Mano224 <mano@example.com>
2024-10-05 23:15:49 +0300

    d

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,21 +1,39 @@
-#!/usr/bin/python3
-#a list of lists of integers representing the Pascal’s triangle of n
+# def permute(string, pocket=""):
+#     if len(string) == 0:
+#         print(pocket)
+#     else:
+#         for i in range(len(string)):
+#             letter = string[i]
+#             front = string[0:i]
+#             back = string[i+1:]
+#             together = front + back
+#             permute(together, letter + pocket)
+
+# print(permute("AB", ""))
+
 def pascal_triangle(n):
     if n <= 0:
         return []
-    
+
+    # Initialize the first row of Pascal's Triangle
     triangle = [[1]]
 
-    for i in range(1,n):
-
+    for i in range(1, n):
+        # Start with 1 for the new row
         row = [1]
 
-        for j in range(1,i):
+        # Compute the values in the middle of the row
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
 
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        # End with 1 for the new row
         row.append(1)
+
+        # Add the row to the triangle
         triangle.append(row)
 
-    return  triangle
-    
-            
+    return triangle
+
+t = pascal_triangle(5)
+for row in t:
+    print(row)
